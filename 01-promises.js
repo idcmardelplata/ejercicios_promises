5db36d9f4281c6b85c7bc1fdd5c9786977110cec
Martin Algañaraz <idcmardelplata@gmail.com>
2025-05-21 15:11:42 -0300

    Third example of promises

--- 01-promises.js
+++ 01-promises.js
@@ -1,24 +1,37 @@
-/*
- * Pending.
- * fullfiled
- * rejected
- */
 
-const promise = new Promise(
-  (resolve) => {
-    setTimeout(() => {
-      resolve("Primer resultado");
-    }, 4000)
-  });
+// const p = Promise.resolve("Promesa resuelta");
 
-promise.then((resultado) => {
-  console.log(resultado);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(resultado + " procesado");
-    }, 500)
-  });
-})
+// p.then(console.log);
+
+// const promiseRejected = Promise.reject("No funciona");
+
+
+// promiseRejected
+//   .then(console.log)
+//   .catch(error => console.error("Fallo: " + error));
+
+const promises = [
+  Promise.reject("First promise"),
+  Promise.reject("Second promise"),
+  Promise.reject("Third promise"),
+  Promise.resolve("Fourty promise"),
+  Promise.reject("Fourty promise"),
+];
+
+// Promise.all: retorna una promesa si todas fueron completadas con exito
+
+// Promise.all(promises)
+//   .then(data => console.log(data));
+
+// Promise.any: Si alguna tiene exito, te retorna el resultado.
+// Promise.any(promises)
+//   .then(data => console.log(data));
+
+
+
+// Promise.allSettled(promises)
+//   .then(console.log)
+
+Promise.race(promises)
   .then(console.log)
   .catch(console.error);
-
